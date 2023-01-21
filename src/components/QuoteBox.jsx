cdfdcc9edbcc7230b9aafaf87487f172bbff40f3
Mateo <miketry21@gmail.com>
2023-01-20 21:29:19 -0500

    primer commit

---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import QuoteButton from './QuoteButton'
+
+const QuoteBox = ({quote, newQuote, color}) => {
+  return (
+    <div className='quoteBox' style={{color: color}}>
+        <i className='bx bxs-quote-left quoteBox_icon'></i>
+        <p className='quoteBox_quote'>{quote.quote}</p>
+        <h3 className='quoteBox_author'>{quote.author}</h3>
+        <QuoteButton color={color} newQuote = {newQuote}/>
+    </div>
+  )
+}
+
+export default QuoteBox
